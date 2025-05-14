67b9be198ac4ca304ba7cd8becda5e2254a9daf0
MateenSQ <111274354+MateenSQ@users.noreply.github.com>
2025-05-14 09:14:42 +0100

    Added test files

---
+++ BusinessReportingMVCTest/Controllers/AdminControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using BusinessReportingMVC.Controllers;
+using BusinessReportingMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessReportingMVCXUnit.Controllers
+{
+    
+    public class AdminControllerTests : ControllerTestBase
+    {
+        [Fact]
+        public async void ManageUsers_ReturnsViewResultWithNoExceptions()
+        {
+            // Arrange
+            var data = new List<PersonalInfoViewModel> 
+            {
+                new() { Id = 1, Email = "user1@email.com", Name = "User1", Approved = true, Position = "Seacht", Role = "Admin" },
+                new() { Id = 1, Email = "user1@email.com", Name = "User1", Approved = true, Position = "MD", Role = "User" }
+            };
+
+            _adminServiceMock.Setup(s => s.GetAllNonAdmins()).ReturnsAsync(data);
+
+            AdminController controller = new AdminController(_adminServiceMock.Object);
+
+            // Act
+            var result = await controller.ManageUsers();
+
+            var exception = await Record.ExceptionAsync(() => controller.ManageUsers());
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            _adminServiceMock.Verify(service => service.GetAllNonAdmins(), Times.Exactly(2));
+            Assert.NotNull(viewResult);
+            Assert.Null(exception);
+        }
+    }
+}
---
+++ BusinessReportingMVCTest/Controllers/ControllerTestBase.cs
@@ -0,0 +1,33 @@
+﻿using BusinessReportingMVC.Data;
+using BusinessReportingMVC.Repositories;
+using BusinessReportingMVC.Services;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessReportingMVCXUnit.Controllers
+{
+    public class ControllerTestBase
+    {
+        public readonly BusinessReportingDbContext _contextMock;
+        public readonly Mock<IAuthService> _authServiceMock;
+        public readonly Mock<IReportService> _reportServiceMock;
+        public readonly Mock<IAdminService> _adminServiceMock;
+        public readonly Mock<IBusinessReportingRepository> _repoMock;
+        public readonly Mock<IHttpContextAccessor> _httpAccessormock;
+
+        public ControllerTestBase()
+        {
+            _reportServiceMock = new Mock<IReportService>(MockBehavior.Strict);
+            _authServiceMock = new Mock<IAuthService>(MockBehavior.Strict);
+            _adminServiceMock = new Mock<IAdminService>(MockBehavior.Strict);
+            _repoMock = new Mock<IBusinessReportingRepository>(MockBehavior.Strict);
+            _httpAccessormock = new Mock<IHttpContextAccessor>(MockBehavior.Strict);
+            _contextMock = new BusinessReportingDbContext();
+        }
+    }
+}
