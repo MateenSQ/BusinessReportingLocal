3acb7d6ca870a53f90750b76f1d0bad5d2ebdbc8
MateenSQ <111274354+MateenSQ@users.noreply.github.com>
2025-05-28 19:00:51 +0100

    some adjustments

--- BusinessReportingMVC/Data/SeedData.cs
+++ BusinessReportingMVC/Data/SeedData.cs
@@ -46,12 +46,28 @@ namespace BusinessReportingMVC.Data
                 ClaimName = "MD"
             });
 
-            _context.Add(new User
-            {
-                UserId = 1,
-                Email = "John@email.com",
-                Name = ""
-            });
+            //_context.Add(new User
+            //{
+            //    UserId = 1,
+            //    Email = "John@email.com",
+            //    Name = "John",
+            //    Password = "HfnHBpXC/v6r1CpPNztP3J7u+IyTrpPwgdVge1V3d+E=",
+            //    Salt = "9pcyevxF1uUR9pPh1dV7lc1yEDcMOYArZXLw3F+nNZc=",
+            //    IsApproved = true,
+            //    Reports = new List<Report>()
+            //    {
+            //        new Report()
+            //        {
+            //            ReportId = 1,
+            //            CreatedByUserId = 1,
+            //            ReportName = "2024 Q1 Report for company X",
+            //            FromDateRange = new DateTime(2024, 1, 1),
+            //            ToDateRange = new DateTime(2024, 3, 31),
+            //            CreatedDate = new DateTime(2024, 2, 20),
+            //            IsDraft = false,
+            //        }
+            //    }
+            //});
 
             _context.SaveChanges();
         }
